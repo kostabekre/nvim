7124eff55967bfcf8b81460521206939fe11164a
kostabekre <kostabekre@gmail.com>
2025-01-13 17:44:35 +0300

    telescope mappings update

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,13 +5,30 @@ return {
         dependencies = {
             'nvim-lua/plenary.nvim'
         },
-        opts = {
-            defaults = {
-                file_ignore_patterns = { ".godot" }
-            }
-        },
         keys = { "<leader>ff", "<leader>fg", "<leader>gf", "<leader>fb", "<leader>fh", "gd", "gt", "gi", "gr" },
         config = function()
+            local telescope = require('telescope')
+
+            telescope.setup{
+                defaults = {
+                    file_ignore_patterns = { ".godot", ".git" },
+                    preview = {
+                        hide_on_startup = true
+                    },
+                    mappings = {
+                        n = {
+                            ["<C-p>"] = require("telescope.actions.layout").toggle_preview,
+                            ["l"] = require('telescope.actions').cycle_history_next,
+                            ["h"] = require('telescope.actions').cycle_history_prev,
+                        },
+                        i = {
+                            ["<C-p>"] = require("telescope.actions.layout").toggle_preview,
+                            -- ["<C-j>"] = require "telescope.actions.layout".toggle_results_and_prompt,
+                        },
+                    }
+                }
+            }
+
             local telescope_builtin = require('telescope.builtin')
 
             keymap('n', '<leader>ff', telescope_builtin.find_files, { desc = "Find files" })
@@ -19,6 +36,7 @@ return {
             keymap('n', '<leader>fg', telescope_builtin.live_grep, { desc = "Find in files" })
             keymap('n', '<leader>fb', telescope_builtin.buffers, { desc = "Find buffers" })
             keymap('n', '<leader>fh', telescope_builtin.help_tags, { desc = "Find help tags" })
+            keymap('n', '<leader>fr', telescope_builtin.resume, { desc = "Resume Telescope Search" })
         end
     }
 }
