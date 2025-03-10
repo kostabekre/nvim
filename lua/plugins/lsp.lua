419f4a4c218bd3d3bd83789d51cfee6736b5b5d4
kostabekre <kostabekre@gmail.com>
2025-03-10 18:02:58 +0300

    switched to roslyn on linux

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -85,25 +85,25 @@ return {
 
             local csharp_ls_extended = require 'csharpls_extended'
 
-            if vim.fn.has('unix') == 1 then
-                lspconfig.csharp_ls.setup {
-                    cmd = { 'csharp-ls' },
-                    capabilities = default_capabilities,
-                    handlers = {
-                        ["textDocument/definition"] = csharp_ls_extended.handler,
-                        ["textDocument/typeDefinition"] = csharp_ls_extended.handler,
-                    },
-                    root_dir = function(fname)
-                        return util.root_pattern '*.sln' (fname) or util.root_pattern '*.csproj' (fname)
-                    end,
-                    filetypes = { 'cs' },
-                    init_options = {
-                        AutomaticWorkspaceInit = true,
-                    },
-                }
-            else
-
-            end
+            -- remporarily comment out chsarp ls to try roslyn 
+            -- if vim.fn.has('unix') == 1 then
+            --     lspconfig.csharp_ls.setup {
+            --         cmd = { 'csharp-ls' },
+            --         capabilities = default_capabilities,
+            --         handlers = {
+            --             ["textDocument/definition"] = csharp_ls_extended.handler,
+            --             ["textDocument/typeDefinition"] = csharp_ls_extended.handler,
+            --         },
+            --         root_dir = function(fname)
+            --             return util.root_pattern '*.sln' (fname) or util.root_pattern '*.csproj' (fname)
+            --         end,
+            --         filetypes = { 'cs' },
+            --         init_options = {
+            --             AutomaticWorkspaceInit = true,
+            --         },
+            --     }
+            -- else
+            -- end
         end
     }
 }
--- lua/plugins/roslyn.lua
+++ lua/plugins/roslyn.lua
@@ -1,15 +1,12 @@
-if vim.fn.has('unix') == 0 then
-    return {
-        {
-            "seblj/roslyn.nvim",
-            ft = "cs",
-            event = { "BufReadPre", "BufNewFile" },
-            opts = {
-                filewatching = false,
-                broad_search = true,
-            },
-        }
+return {
+    {
+        "seblj/roslyn.nvim",
+        ft = "cs",
+        event = { "BufReadPre", "BufNewFile" },
+        opts = {
+            filewatching = false,
+            broad_search = true,
+        },
     }
-else
-    return {}
-end
+
+}
