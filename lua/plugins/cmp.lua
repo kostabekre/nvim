d947298df689e0a3843245cc295ef75849a8b542
kostabekre <kostabekre@gmail.com>
2025-03-27 14:00:36 +0300

    cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -57,15 +57,15 @@ return {
             })
 
             -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-            --cmp.setup.cmdline(':', {
-            --    mapping = cmp.mapping.preset.cmdline(),
-            --    sources = cmp.config.sources({
-            --        { name = 'path' }
-            --    }, {
-            --        { name = 'cmdline' }
-            --    }),
-            --    matching = { disallow_symbol_nonprefix_matching = false }
-            --})
+            cmp.setup.cmdline(':', {
+               mapping = cmp.mapping.preset.cmdline(),
+               sources = cmp.config.sources({
+                   { name = 'path' }
+               }, {
+                   { name = 'cmdline' }
+               }),
+               matching = { disallow_symbol_nonprefix_matching = false }
+            })
 
             -- TODO what is happening
             -- luasnip.config.set_config {
