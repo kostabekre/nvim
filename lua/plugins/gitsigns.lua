ef0f0b90dd9a83061823bde92a341bf145d21262
kostabekre <kostabekre@gmail.com>
2025-05-05 10:52:37 +0300

    git signs changes

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -3,6 +3,8 @@ return {
 		"lewis6991/gitsigns.nvim",
 		config = function()
 			require("gitsigns").setup()
+			vim.keymap.set("n", "<leader>an", "<cmd>Gitsigns blame<CR>")
+			vim.keymap.set("n", "<leader>al", "<cmd>Gitsigns blame_line<CR>")
 		end,
 		event = "BufReadPre",
 	},
