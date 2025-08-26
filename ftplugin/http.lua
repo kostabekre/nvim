f318b485b952cb338d752fc281ec76bcb4019086
kostabekre <kostabekre@gmail.com>
2025-08-26 15:44:27 +0300

    change http, json

--- ftplugin/http.lua
+++ ftplugin/http.lua
@@ -1,27 +1,31 @@
 -- config in lazy is not called, so make keybindings here
-require("telescope").load_extension("rest")
+-- not enabled, because testing kulala
+local function add_rest()
+	require("telescope").load_extension("rest")
 
--- local wk = require("which-key")
--- wk.add({
--- 	{ "<CR>", "<CMD>Rest run<CR>", desc = "Run Rest", mode = "n", group = "Rest" },
--- 	{ "<leader>ro", "<CMD>Rest logs<CR>", desc = "Show Rest Logs", mode = "n", group = "Rest" },
--- 	{ "<leader>ro", "<CMD>Rest logs<CR>", desc = "Rest Run Last", mode = "n", group = "Rest" },
--- 	{ "<leader>rc", "<CMD>Rest cookies<CR>", desc = "Show Rest Cookies", mode = "n", group = "Rest" },
--- 	{
--- 		"<leader>re",
--- 		function()
--- 			require("telescope").extensions.rest.select_env()
--- 		end,
--- 		desc = "Rest Show environment",
--- 		mode = "n",
--- 		group = "Rest",
--- 	},
--- })
+	vim.keymap.set("n", "<CR>", "<CMD>Rest run<CR>", { desc = "Run Rest" })
+	vim.keymap.set("n", "<leader>ro", "<CMD>Rest logs<CR>", { desc = "Show Rest Logs" })
+	vim.keymap.set("n", "<leader>ra", "<CMD>Rest last<CR>", { desc = "Rest Run Last" })
+	vim.keymap.set("n", "<leader>rc", "<CMD>Rest cookies<CR>", { desc = "Show Rest Cookies" })
+	vim.keymap.set("n", "<leader>re", function()
+		require("telescope").extensions.rest.select_env()
+	end, { desc = "Show environment" })
 
-vim.keymap.set("n", "<CR>", "<CMD>Rest run<CR>", { desc = "Run Rest" })
-vim.keymap.set("n", "<leader>ro", "<CMD>Rest logs<CR>", { desc = "Show Rest Logs" })
-vim.keymap.set("n", "<leader>ra", "<CMD>Rest last<CR>", { desc = "Rest Run Last" })
-vim.keymap.set("n", "<leader>rc", "<CMD>Rest cookies<CR>", { desc = "Show Rest Cookies" })
-vim.keymap.set("n", "<leader>re", function()
-	require("telescope").extensions.rest.select_env()
-end, { desc = "Show environment" })
+	-- uncomment if you want to make it work
+	-- local wk = require("which-key")
+	-- wk.add({
+	-- 	{ "<CR>", "<CMD>Rest run<CR>", desc = "Run Rest", mode = "n", group = "Rest" },
+	-- 	{ "<leader>ro", "<CMD>Rest logs<CR>", desc = "Show Rest Logs", mode = "n", group = "Rest" },
+	-- 	{ "<leader>ro", "<CMD>Rest logs<CR>", desc = "Rest Run Last", mode = "n", group = "Rest" },
+	-- 	{ "<leader>rc", "<CMD>Rest cookies<CR>", desc = "Show Rest Cookies", mode = "n", group = "Rest" },
+	-- 	{
+	-- 		"<leader>re",
+	-- 		function()
+	-- 			require("telescope").extensions.rest.select_env()
+	-- 		end,
+	-- 		desc = "Rest Show environment",
+	-- 		mode = "n",
+	-- 		group = "Rest",
+	-- 	},
+	-- })
+end
--- ftplugin/json.lua
+++ ftplugin/json.lua
@@ -1,1 +1,4 @@
 vim.bo.formatprg = "jq"
+vim.opt_local.shiftwidth = 2
+vim.opt_local.tabstop = 2
+vim.opt_local.softtabstop = 2
