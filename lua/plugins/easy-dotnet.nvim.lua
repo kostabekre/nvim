c3624514cd31833a87e3e3e9acf2b8864383fa20
kostabekre <kostabekre@gmail.com>
2025-08-28 18:02:13 +0300

    update easy dotnet

--- lua/plugins/easy-dotnet.nvim.lua
+++ lua/plugins/easy-dotnet.nvim.lua
@@ -2,8 +2,19 @@ return {
 	{
 		"GustavEikaas/easy-dotnet.nvim",
 		dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+		dir = "~/source/neovim/plugins/easy-dotnet.nvim",
 		config = function()
-			require("easy-dotnet").setup()
+			require("easy-dotnet").setup({
+				auto_bootstrap_namespace = {
+					--block_scoped, file_scoped
+					type = "file_scoped",
+					enabled = true,
+					use_clipboard_json = {
+						behavior = "prompt", --'auto' | 'prompt' | 'never',
+						register = "+", -- which register to check
+					},
+				},
+			})
 		end,
 	},
 }
