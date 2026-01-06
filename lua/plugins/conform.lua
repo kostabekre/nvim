e3978cb278cab699d11ad822d6de668c0b780fe4
kostabekre <kostabekre@gmail.com>
2026-01-06 13:46:32 +0300

    disabled gdformat due to slow saving

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,46 +1,46 @@
 -- Formats a file with given formatters or with LSP if there is no formatter available.
 
 return {
-	"stevearc/conform.nvim",
-	ft = {
-		"lua",
-		"cs",
-		"typescriptreact",
-		"python",
-		"javascript",
-		"gdscript",
-	},
-	config = function()
-		require("conform").setup({
-			-- PAY ATTENTION at the ft above, if you use new language
-			formatters_by_ft = {
-				lua = { "stylua" },
-				--cs = { "csharpier" }, -- doesn't work, so it's fallbacked to LSP (roslyn)
-				typescriptreact = { "prettierd", "prettier", stop_after_first = true },
-				-- Conform will run multiple formatters sequentially
-				python = { "isort", "black" },
-				-- You can customize some of the format options for the filetype (:help conform.format)
-				-- rust = { "rustfmt", lsp_format = "fallback" },
-				-- Conform will run the first available formatter
-				javascript = { "prettierd", "prettier", stop_after_first = true },
-				gdscript = { "gdformat" },
-			},
-			formatters = {
-				stylua = {
-					cwd = require("conform.util").root_file({ ".stylua.toml" }),
-				},
-				csharpier = {
-					command = "csharpier",
-					args = { "format", "$FILENAME", "--write-stdout" },
-				},
-				cwd = require("conform.util").root_file({ ".gitignore" }),
-			},
-			format_on_save = {
-				-- These options will be passed to conform.format()
-				timeout_ms = 500,
-				lsp_format = "fallback",
-			},
-			log_level = vim.log.levels.ERROR,
-		})
-	end,
+    "stevearc/conform.nvim",
+    ft = {
+        "lua",
+        "cs",
+        "typescriptreact",
+        "python",
+        "javascript",
+    },
+    config = function()
+        require("conform").setup({
+            -- PAY ATTENTION at the ft above, if you use new language
+            formatters_by_ft = {
+                lua = { "stylua" },
+                --cs = { "csharpier" }, -- doesn't work, so it's fallbacked to LSP (roslyn)
+                typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+                -- Conform will run multiple formatters sequentially
+                python = { "isort", "black" },
+                -- You can customize some of the format options for the filetype (:help conform.format)
+                -- rust = { "rustfmt", lsp_format = "fallback" },
+                -- Conform will run the first available formatter
+                javascript = { "prettierd", "prettier", stop_after_first = true },
+                -- disabled because save becomes slow, it's better to use in precommit instead
+                -- gdscript = { "gdformat" },
+            },
+            formatters = {
+                stylua = {
+                    cwd = require("conform.util").root_file({ ".stylua.toml" }),
+                },
+                csharpier = {
+                    command = "csharpier",
+                    args = { "format", "$FILENAME", "--write-stdout" },
+                },
+                cwd = require("conform.util").root_file({ ".gitignore" }),
+            },
+            format_on_save = {
+                -- These options will be passed to conform.format()
+                timeout_ms = 500,
+                lsp_format = "fallback",
+            },
+            log_level = vim.log.levels.ERROR,
+        })
+    end,
 }
