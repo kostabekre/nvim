a9b8714f66abbd1820bccb201ba34e65892309a5
kostabekre <kostabekre@gmail.com>
2026-02-03 15:56:32 +0300

    changed to dap

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -3,16 +3,10 @@ return {
         "mfussenegger/nvim-dap",
         dependencies = {
             "rcarriga/nvim-dap-ui",
-
-            -- Required dependency for nvim-dap-ui
             "nvim-neotest/nvim-nio",
 
-            -- Installs the debug adapters for you
             "williamboman/mason.nvim",
             "jay-babu/mason-nvim-dap.nvim",
-
-            -- Add your own debuggers here
-            -- 'leoluz/nvim-dap-go',
         },
         keys = {
             "<F5>",
@@ -36,28 +30,48 @@ return {
                 return
             end
 
+            -- Keymaps for controlling the debugger
+            vim.keymap.set("n", "<leader>dq", function()
+                dap.terminate()
+                dap.clear_breakpoints()
+            end, { desc = "Terminate and clear breakpoints" })
+
+            vim.keymap.set("n", "<F5>", dap.continue, { desc = "Start/continue debugging DAP" })
+            vim.keymap.set("n", "<F10>", dap.step_over, { desc = "Step over DAP" })
+            vim.keymap.set("n", "<F11>", dap.step_into, { desc = "Step into DAP" })
+            vim.keymap.set("n", "<F12>", dap.step_out, { desc = "Step out DAP" })
+            vim.keymap.set("n", "<leader>B", dap.toggle_breakpoint, { desc = "Toggle breakpoint DAP" })
+            vim.keymap.set("n", "<leader>dO", dap.step_over, { desc = "Step over (alt) DAP" })
+            vim.keymap.set("n", "<leader>dC", dap.run_to_cursor, { desc = "Run to cursor DAP" })
+            vim.keymap.set("n", "<leader>dr", dap.repl.toggle, { desc = "Toggle DAP REPL" })
+            vim.keymap.set("n", "<leader>dj", dap.down, { desc = "Go down stack frame DAP" })
+            vim.keymap.set("n", "<leader>dk", dap.up, { desc = "Go up stack frame DAP" })
+
+            -- Inspecting state
+            vim.keymap.set("n", "<leader>sk", function()
+                require("dap.ui.widgets").hover()
+            end, { desc = "Value of expression under cursor DAP" })
+
             local status_ok_ui, dapui = pcall(require, "dapui")
             if not status_ok_ui then
                 print("nvim-dap is not installed!")
                 return
             end
 
-            -- Keymaps for controlling the debugger
-            vim.keymap.set("n", "dq", function()
-                dap.terminate()
-                dap.clear_breakpoints()
-            end, { desc = "Terminate and clear breakpoints" })
+            require("dapui").setup()
 
-            vim.keymap.set("n", "<F5>", dap.continue, { desc = "Start/continue debugging" })
-            vim.keymap.set("n", "<F10>", dap.step_over, { desc = "Step over" })
-            vim.keymap.set("n", "<F11>", dap.step_into, { desc = "Step into" })
-            vim.keymap.set("n", "<F12>", dap.step_out, { desc = "Step out" })
-            vim.keymap.set("n", "<leader>B", dap.toggle_breakpoint, { desc = "Toggle breakpoint" })
-            vim.keymap.set("n", "<leader>dO", dap.step_over, { desc = "Step over (alt)" })
-            vim.keymap.set("n", "<leader>dC", dap.run_to_cursor, { desc = "Run to cursor" })
-            vim.keymap.set("n", "<leader>dr", dap.repl.toggle, { desc = "Toggle DAP REPL" })
-            vim.keymap.set("n", "<leader>dj", dap.down, { desc = "Go down stack frame" })
-            vim.keymap.set("n", "<leader>dk", dap.up, { desc = "Go up stack frame" })
+            dap.listeners.before.attach.dapui_config = function()
+                dapui.open()
+            end
+            dap.listeners.before.launch.dapui_config = function()
+                dapui.open()
+            end
+            dap.listeners.before.event_terminated.dapui_config = function()
+                dapui.close()
+            end
+            dap.listeners.before.event_exited.dapui_config = function()
+                dapui.close()
+            end
         end,
     },
 }
