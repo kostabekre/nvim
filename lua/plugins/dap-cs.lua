4d0a773edf08bfbd95126ff35e53c25abdc55ed3
kostabekre <kostabekre@gmail.com>
2026-04-05 15:58:27 +0300

    fix indent

--- lua/plugins/dap-cs.lua
+++ lua/plugins/dap-cs.lua
@@ -1,21 +1,17 @@
 return {
-  "nicholasmata/nvim-dap-cs",
-  dependencies = { "mfussenegger/nvim-dap", "mason-org/mason.nvim" },
-  ft = {
-    "cs",
-  },
-  config = function()
-    local mason_registry = require("mason-registry")
-    local have_netcoredbg = mason_registry.is_installed("netcoredbg")
+    "nicholasmata/nvim-dap-cs",
+    dependencies = { "mfussenegger/nvim-dap", "mason-org/mason.nvim" },
+    ft = {
+        "cs",
+    },
+    config = function()
+        local mason_registry = require("mason-registry")
+        local have_netcoredbg = mason_registry.is_installed("netcoredbg")
 
-    if have_netcoredbg then
-      require("dap-cs").setup({
-        netcorebg = {
-          path = vim.fn.expand("$MASON/bin/netcoredbg"),
-        },
-      })
-    else
-      print("netcoredbg is not installed!")
-    end
-  end,
+        if have_netcoredbg then
+            require("dap-cs").setup()
+        else
+            print("netcoredbg is not installed!")
+        end
+    end,
 }
