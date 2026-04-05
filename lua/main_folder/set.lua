34b2cab148a0bdb1f2e846e2a9c75add03bebbe4
kostabekre <kostabekre@gmail.com>
2026-04-05 15:20:27 +0300

    fixed error with locale

--- lua/main_folder/set.lua
+++ lua/main_folder/set.lua
@@ -1,30 +1,30 @@
 local options = {
-	nu = true,
-	relativenumber = true,
-	tabstop = 4, -- insert n spaces for a tab character
-	softtabstop = 4, -- insert n spaces on Tab key or remove n spaces on Backspace key
-	shiftwidth = 4, -- insert n columns for each indentation
-	expandtab = true, -- convert tabs to spaces
-	smartindent = true,
-	wrap = false, -- display a line as multiple/one long line
-	swapfile = false, -- creates a swapfile
-	backup = false, -- creates a backup
-	undofile = true, -- creates an undo file
-	hlsearch = false,
-	incsearch = true,
-	termguicolors = true,
-	scrolloff = 8,
-	signcolumn = "yes",
-	mouse = "a",
-	updatetime = 50,
-	-- colorcolumn = "80", it's better to use only for specific languages
+    nu = true,
+    relativenumber = true,
+    tabstop = 4, -- insert n spaces for a tab character
+    softtabstop = 4, -- insert n spaces on Tab key or remove n spaces on Backspace key
+    shiftwidth = 4, -- insert n columns for each indentation
+    expandtab = true, -- convert tabs to spaces
+    smartindent = true,
+    wrap = false, -- display a line as multiple/one long line
+    swapfile = false, -- creates a swapfile
+    backup = false, -- creates a backup
+    undofile = true, -- creates an undo file
+    hlsearch = false,
+    incsearch = true,
+    termguicolors = true,
+    scrolloff = 8,
+    signcolumn = "yes",
+    mouse = "a",
+    updatetime = 50,
+    -- colorcolumn = "80", it's better to use only for specific languages
 }
 
 for key, value in pairs(options) do
-	vim.opt[key] = value
+    vim.opt[key] = value
 end
 
-vim.cmd("language en_US")
+vim.cmd("language en_US.utf8")
 
 -- https://github.com/NvChad/NvChad/issues/1907
 vim.hl.priorities.semantic_tokens = 95 -- Or any number lower than 100, treesitter's priority level
@@ -34,15 +34,15 @@ vim.opt.undodir = vim.fn.stdpath("data") .. "/undodir"
 vim.opt.isfname:append("@-@")
 
 function ColorMyEditor(color, transparent)
-	color = color or "default"
-	local status_ok, _ = pcall(vim.cmd, "colorscheme " .. color)
-	if not status_ok then
-		print("color scheme " .. color .. " is not found!")
-		return
-	end
+    color = color or "default"
+    local status_ok, _ = pcall(vim.cmd, "colorscheme " .. color)
+    if not status_ok then
+        print("color scheme " .. color .. " is not found!")
+        return
+    end
 
-	if transparent then
-		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-	end
+    if transparent then
+        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    end
 end
