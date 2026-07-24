b5d9712f5f5aa952c0dff65c1ff13bb00dd5e785
kostabekre <kostabekre@gmail.com>
2026-07-24 12:23:51 +0300

    added new mapping for obsidian-nvim

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -3,3 +3,7 @@ vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.opt.foldtext = ""
 vim.opt.foldlevel = 99
+
+vim.keymap.set("n", "<leader>oc", function()
+    vim.api.nvim_cmd({ cmd = "ObsidianBridgeOpenCurrentActiveFile" }, {})
+end, { desc = "Open current obsidian note in neovim" })
