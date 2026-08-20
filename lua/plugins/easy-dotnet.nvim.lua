36a46c2b2cc74db169cf9d86ea9e8aed3d47f6ba
kostabekre <kostabekre@gmail.com>
2026-08-20 09:07:04 +0300

    added safe check in easy-dotnet plugin

--- lua/plugins/easy-dotnet.nvim.lua
+++ lua/plugins/easy-dotnet.nvim.lua
@@ -1,34 +1,38 @@
 return {
-	{
-		"GustavEikaas/easy-dotnet.nvim",
-		dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
-		enabled = false,
-		-- why stopped working?
-		cond = function(_)
-			local util = require("lspconfig.util")
-			local pattern_func = util.root_pattern("*.sln", ".csproj")
+    {
+        "GustavEikaas/easy-dotnet.nvim",
+        dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+        enabled = false,
+        -- why stopped working?
+        cond = function(_)
+            local status_util, util = pcall(require, "lspconfig.util")
+            if not status_util then
+                return false
+            end
 
-			local root = pattern_func(vim.uv.cwd())
-			if not root then
-				return false
-			end
+            local pattern_func = util.root_pattern("*.sln", ".csproj")
 
-			return true
-		end,
-		config = function()
-			require("easy-dotnet").setup({
-				auto_bootstrap_namespace = {
-					--block_scoped, file_scoped
-					type = "file_scoped",
-					enabled = true,
-					use_clipboard_json = {
-						behavior = "prompt", --'auto' | 'prompt' | 'never',
-						register = "+", -- which register to check
-					},
-				},
-			})
-			vim.keymap.set("n", "<leader>Nt", ":Dotnet testrunner refresh<CR>")
-			vim.keymap.set("n", "<leader>Nrt", ":Dotnet testrunner<CR>")
-		end,
-	},
+            local root = pattern_func(vim.uv.cwd())
+            if not root then
+                return false
+            end
+
+            return true
+        end,
+        config = function()
+            require("easy-dotnet").setup({
+                auto_bootstrap_namespace = {
+                    --block_scoped, file_scoped
+                    type = "file_scoped",
+                    enabled = true,
+                    use_clipboard_json = {
+                        behavior = "prompt", --'auto' | 'prompt' | 'never',
+                        register = "+", -- which register to check
+                    },
+                },
+            })
+            vim.keymap.set("n", "<leader>Nt", ":Dotnet testrunner refresh<CR>")
+            vim.keymap.set("n", "<leader>Nrt", ":Dotnet testrunner<CR>")
+        end,
+    },
 }
